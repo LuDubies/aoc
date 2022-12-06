5691fa0f4456f31bf74f2994179d3e1c301b1f48
Luca <luca@noelscher.com>
2022-12-06 18:06:05 +0100

    add day6 solution

--- day6/message.py
+++ day6/message.py
@@ -1,3 +1,14 @@
+def all_unique(snippet: str) -> bool:
+    return all(c not in snippet[:i:] + snippet[i+1::] for i, c in enumerate(snippet))
+
+
 with open('input.txt', 'r') as inp:
-    lines = inp.readlines()
+    line = inp.readlines()[0]
+
+    print(f"Start of packet at {[ind + 4 for ind, _ in enumerate(line) if all_unique(line[ind:ind+4:])][0]}.")
+    print(f"Start of message after {[ind + 14 for ind, _ in enumerate(line) if all_unique(line[ind:ind+14:])][0]}.")
+
+
+
+
 
