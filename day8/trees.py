b734037e597ae1ae7aa9e216e716b0dc20f49111
Luca <luca@noelscher.com>
2022-12-08 08:52:57 +0100

    little more progress on day8

--- day8/trees.py
+++ day8/trees.py
@@ -6,5 +6,13 @@ with open('input.txt', 'r') as inp:
     tree_map = [[int(tree) for tree in list(line.strip())] for line in lines]
 
     highest = - 1
+
+    # visible for the left
     left_visible = [[highest := tree if i == 0 else max(highest, tree) for i, tree in enumerate(treeline)] for treeline in tree_map]
-    print(left_visible)
+    left_visible = [[True] + [tree > treeline[i] for i, tree in enumerate(treeline[1::])] for treeline in left_visible]
+
+    # visible from the right
+    right_visible = [[highest := tree if i == 0 else max(highest, tree) for i, tree in enumerate(reversed(treeline))] for treeline in tree_map]
+    right_visible = [[True] + [tree > treeline[i] for i, tree in enumerate(treeline[1::])] for treeline in right_visible]
+    right_visible = [list(reversed(treeline)) for treeline in right_visible]
+    
