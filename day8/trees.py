bac39fac874f8a3bc1f6775a7222416c3a9e8090
Luca <luca@noelscher.com>
2022-12-08 09:37:52 +0100

    switched to numpy solution, second star challenge still has bugs/ unsolved

--- day8/trees.py
+++ day8/trees.py
@@ -1,18 +1,35 @@
+import numpy as np
 
+
+def visible(x: int, y: int, tmap: np.array) -> bool:
+    xcnt, ycnt = tmap.shape
+
+    if x == 0 or y == 0 or x == xcnt - 1 or y == ycnt - 1:
+        return True
+    return np.all(np.less(tmap[x, :y], tmap[x, y])) or np.all(np.less(tmap[x, y + 1:], tmap[x, y])) or \
+           np.all(np.less(tmap[:x, y], tmap[x, y])) or np.all(np.less(tmap[x+1:, y], tmap[x, y]))
+
+
+def view_distance(height: int, treeline: np.array) -> int:
+    if treeline.size == 0:
+        return 0
+    overlook = np.less(treeline, height)
+    if np.all(overlook):
+        return overlook.size
+    return np.nonzero(np.invert(overlook))[0][0] + 1
+
+
+def scenic_score(x: int, y: int, tmap: np.array) -> int:
+    tv = view_distance(tmap[x, y], np.flip(tmap[:x, y]))
+    bv = view_distance(tmap[x, y], tmap[x+1:, y])
+    lv = view_distance(tmap[x, y], np.flip(tmap[x, :y]))
+    rv = view_distance(tmap[x, y], tmap[x, y+1:])
+    # print(f"Scenic score of {x}, {y} is {tv} * {bv} * {lv} * {rv} = {tv * bv * lv * rv}.")
 
 with open('input.txt', 'r') as inp:
     lines = inp.readlines()
 
     tree_map = [[int(tree) for tree in list(line.strip())] for line in lines]
-
-    highest = - 1
-
-    # visible for the left
-    left_visible = [[highest := tree if i == 0 else max(highest, tree) for i, tree in enumerate(treeline)] for treeline in tree_map]
-    left_visible = [[True] + [tree > treeline[i] for i, tree in enumerate(treeline[1::])] for treeline in left_visible]
-
-    # visible from the right
-    right_visible = [[highest := tree if i == 0 else max(highest, tree) for i, tree in enumerate(reversed(treeline))] for treeline in tree_map]
-    right_visible = [[True] + [tree > treeline[i] for i, tree in enumerate(treeline[1::])] for treeline in right_visible]
-    right_visible = [list(reversed(treeline)) for treeline in right_visible]
-    
+    tree_map = np.array(tree_map)
+    print(f"Visible tree count is {sum(visible(i, j, tree_map) for i in range(tree_map.shape[0]) for j in range(tree_map.shape[1]))}")
+    print(f"Gighest scenic score is {max(scenic_score(i, j, tree_map) for i in range(tree_map.shape[0]) for j in range(tree_map.shape[1]))}")
