c9ae0dcee0c348b59839a20657015f8551df3d4e
Luca <luca@noelscher.com>
2022-12-09 14:14:41 +0100

    adding class based solution for day9

--- day9/rope_simulator.py
+++ day9/rope_simulator.py
@@ -1,56 +1,64 @@
-from typing import Tuple
-
-TAIL_VISITED = set()
-
-
-def make_step(head: Tuple[int, int], tail: Tuple[int, int], direction: str) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-    hx, hy = head
-    tx, ty = tail
-    match direction:
-        case 'U':
-            hx += 1
-        case 'D':
-            hx -= 1
-        case 'R':
-            hy += 1
-        case 'L':
-            hy -= 1
-
-    # does the tail move?
-    xdiff = hx - tx
-    ydiff = hy - ty
-    need_move = abs(xdiff) > 1 or abs(ydiff) > 1
-
-    if need_move:
-        if xdiff < 0:
-            tx -= 1
-        elif xdiff > 0:
-            tx += 1
-        if ydiff < 0:
-            ty -= 1
-        elif ydiff > 0:
-            ty += 1
-
-    TAIL_VISITED.add((tx, ty))
-    return (hx, hy), (tx, ty)
+from typing import Tuple, List, Union
+from dataclasses import dataclass
 
 
-def make_move(head: Tuple[int, int], tail: Tuple[int, int], move: Tuple[str, int]) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+@dataclass
+class Knot:
+    x: int
+    y: int
+    parent: Union['Knot', None]
+    visited: set
+
+    def move(self, direction: str):
+        match direction:
+            case'U':
+                self.x += 1
+            case 'D':
+                self.x -= 1
+            case 'R':
+                self.y += 1
+            case 'L':
+                self.y -= 1
+        self.visited.add((self.x, self.y))
+
+    def follow(self):
+        xdiff = self.parent.x - self.x
+        ydiff = self.parent.y - self.y
+        if abs(xdiff) > 1 or abs(ydiff) > 1:
+            if xdiff < 0:
+                self.x -= 1
+            if xdiff > 0:
+                self.x += 1
+            if ydiff < 0:
+                self.y -= 1
+            if ydiff > 0:
+                self.y += 1
+        self.visited.add((self.x, self.y))
+
+    @property
+    def pos(self):
+        return self.x, self.y
+
+
+def make_move(rope: List[Knot], move: Tuple[str, int]) -> List[Knot]:
     direction, times = move
     for _ in range(times):
-        head, tail = make_step(head, tail, direction)
-    print(f"After move {move}, head is at {head}, tail is at {tail}.")
-    return head, tail
+        rope[0].move(direction)
+        for k in rope[1:]:
+            k.follow()
+    print(f"After move {move}, head is at {rope[0].pos}, tail is at {rope[-1].pos}.")
+    return rope
 
 
 with open('input.txt', 'r') as inp:
     moves = [(line.strip().split()[0], int(line.strip().split()[1])) for line in inp.readlines()]
-
-    h = (0, 0)
-    t = (0, 0)
+    ROPE_LENGTH = 10
+    rope = [Knot(0, 0, None, set())]
+    for i in range(1, ROPE_LENGTH):
+        rope.append(Knot(0, 0, rope[i-1], set()))
     for move in moves:
-        h, t = make_move(h, t, move)
-    print(len(TAIL_VISITED))
+        rope = make_move(rope, move)
+    print(len(rope[-1].visited))
 
 
 
