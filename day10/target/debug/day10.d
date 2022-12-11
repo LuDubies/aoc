d2b967ea47d7a5e3b4187ae652949dcda3f16eb2
Luca <luca@noelscher.com>
2022-12-11 13:28:39 +0100

    adjusted .gitignore for Rust, cleaned up added garbage from day10

--- day10/target/debug/day10.d
+++
@@ -1,1 +0,0 @@
-C:\Users\lucad\Repos\aoc22\day10\target\debug\day10.exe: C:\Users\lucad\Repos\aoc22\day10\src\main.rs
--- day10/target/debug/deps/day10.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lucad\Repos\aoc22\day10\target\debug\deps\day10.exe: src\main.rs
-
-C:\Users\lucad\Repos\aoc22\day10\target\debug\deps\day10.d: src\main.rs
-
-src\main.rs:
