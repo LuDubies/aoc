aed9ede46943eb0524bf8cd6c0ce42c814e2469d
Luca <luca@noelscher.com>
2022-12-12 21:11:03 +0100

    changing day12 to solve part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -14,7 +14,7 @@ struct Node {
 impl Node {
     fn new(height: char) -> Self{
         if height == 'S' {
-            Self { height: 'a', neighbours: Vec::new(), cost: 0}
+            Self { height: 'a', neighbours: Vec::new(), cost: INFINITE_COST}
         } else if height == 'E' {
             Self {height: 'z', neighbours: Vec::new(), cost: INFINITE_COST}
         } else {
@@ -31,50 +31,17 @@ fn can_climb(from: usize, to: usize, nds: &Vec<Node>) -> bool {
     nds[from].height as usize + 1 >= nds[to].height as usize
 }
 
-fn main() -> Result<()> {
-    let file = File::open("input.txt")?;
-    let reader = BufReader::new(file);
-
-    let lines: Vec<String> = reader.lines().map(|l| l.expect("Could not read line")).collect();
-    
-    let mut node_counter = 0;
-    let mut nodes: Vec<Node> = Vec::new();
-    let mut start: usize = 0;
-    let mut end: usize = 0;
-
-    let grid_height: usize = lines.len();
-    let grid_width: usize = lines[0].len();
-
-    // parse input into node struct
-    for line in lines {
-        for c in line.chars() {
-            if c == 'S' { start = node_counter; }
-            if c == 'E' { end = node_counter; }
-            nodes.push(Node::new(c));
-            node_counter = node_counter + 1;
+fn reset_costs(start: usize, nodes: &mut Vec<Node>) -> () {
+    for i in 0..nodes.len() {
+        if i == start {
+            nodes[i].cost = 0;
+        } else {
+            nodes[i].cost = INFINITE_COST;
         }
     }
+}
 
-    println!("Parsed {node_counter} nodes.");
-
-
-    for id in 0..node_counter {
-        if id % grid_width != 0 && can_climb(id, id-1, &nodes) { 
-            nodes[id].add_neighbour(id - 1);
-         }
-        if id % grid_width != grid_width - 1 && can_climb(id, id+1, &nodes) { 
-            nodes[id].add_neighbour(id + 1);
-         }
-        if id + grid_width < node_counter  && can_climb(id, id + grid_width, &nodes) {
-             nodes[id].add_neighbour(id + grid_width);
-        }
-        if id > grid_width && can_climb(id, id - grid_width, &nodes){ 
-            nodes[id].add_neighbour(id - grid_width); 
-        }
-    }
-
-
-    // dijkstra on the fly
+fn dijkstra(start: usize, end: usize, nodes: &mut Vec<Node>) -> usize {
     let mut queue: VecDeque<usize> = VecDeque::new();
     queue.push_back(start);
 
@@ -94,8 +61,60 @@ fn main() -> Result<()> {
             },
         }
     }
+    nodes[end].cost
 
-    println!("{}", nodes[end].cost);
+}
+
+fn main() -> Result<()> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let lines: Vec<String> = reader.lines().map(|l| l.expect("Could not read line")).collect();
+    
+    let mut node_counter = 0;
+    let mut nodes: Vec<Node> = Vec::new();
+    let mut starts: Vec<usize> = Vec::new();
+    let mut end: usize = 0;
+
+    let grid_height: usize = lines.len();
+    let grid_width: usize = lines[0].len();
+
+    // parse input into node struct
+    for line in lines {
+        for c in line.chars() {
+            if c == 'S' || c == 'a' { starts.push(node_counter); }
+            if c == 'E' { end = node_counter; }
+            nodes.push(Node::new(c));
+            node_counter = node_counter + 1;
+        }
+    }
+
+    println!("Parsed {node_counter} nodes.");
+
+    // set neighbours
+    for id in 0..node_counter {
+        if id % grid_width != 0 && can_climb(id, id-1, &nodes) { 
+            nodes[id].add_neighbour(id - 1);
+         }
+        if id % grid_width != grid_width - 1 && can_climb(id, id+1, &nodes) { 
+            nodes[id].add_neighbour(id + 1);
+         }
+        if id + grid_width < node_counter  && can_climb(id, id + grid_width, &nodes) {
+             nodes[id].add_neighbour(id + grid_width);
+        }
+        if id > grid_width && can_climb(id, id - grid_width, &nodes){ 
+            nodes[id].add_neighbour(id - grid_width); 
+        }
+    }
+
+    let mut min_path_len = INFINITE_COST;
+    for i in 0..starts.len() {
+        reset_costs(starts[i], &mut nodes);
+        let cpl = dijkstra(starts[i], end, &mut nodes);
+        if cpl < min_path_len { min_path_len = cpl; }
+    }
+    
+    println!("Minimum Path is {min_path_len}.");
 
 
     
